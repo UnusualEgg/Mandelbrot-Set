fe087784580431ed8b80a6ac0adbdb681bbc580e
luna <shotgun282006@gmail.com>
2022-08-27 01:08:04 -0500

    Single threaded and added input and generally fixed drawing

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mandelbrot Set in Rust
+
+Use 'z' to prompt the terminal for an exact zoom level
+
+Use 'p' to prompt the terminal for an exact pan
--- src/main.rs
+++ src/main.rs
@@ -1,129 +1,203 @@
-use std::alloc::dealloc;
-use std::collections::HashMap;
-use std::ops::DerefMut;
-use std::sync::{Arc, mpsc, Mutex};
-use sdl2::pixels::{Color, PixelFormat};
+use std::io::{stdout, Write};
+use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use std::{mem, thread};
-use std::thread::JoinHandle;
-use sdl2::libc::{abs, c_int, exit, tm};
 use sdl2::rect::Point;
-use sdl2::surface::Surface;
+use sdl2::render::WindowCanvas;
 
-const width:usize = 500;
-const uwidth:u32 = width as u32;
-const height:usize = 500;
-const uheight:u32 = height as u32;
-
-const size:usize = width*height;
+const WIDTH:usize = 500;
+const UWIDTH:u32 = WIDTH as u32;
+const HEIGHT:usize = 500;
+const UHEIGHT:u32 = HEIGHT as u32;
 
 
 fn main() {
+    //basic setup
     let sdl_context = sdl2::init().expect("Sdl init failed.");
-    let video_subsystem = sdl_context.video().expect("VideoSubsytem failed");
-
-    let window = video_subsystem.window("rust-sdl2 hello", uwidth, uheight)
+    let mut events = sdl_context.event_pump().expect("Could not acquire events?");
+    let video_subsystem = sdl_context.video().expect("VideoSubsystem failed");
+    let window = video_subsystem.window("rust-sdl2 hello", UWIDTH, UHEIGHT)
         .position_centered()
         .build()
         .expect("could not initialize video subsystem");
-
-    let mut canvas = window.into_canvas().build().expect("coudn't make canvas T-T");
-
+    let mut canvas = window.into_canvas().build().expect("couldn't make canvas T-T");
     canvas.set_draw_color(Color::RGB(0,0,0));
     canvas.clear();
     canvas.present();
+    let mut screen = ScreenWindow::new(-2.0, 2.0, -2.0, 2.0);
 
-    let mut events = sdl_context.event_pump().expect("Could not aquire events?");
+
+    //variables
+    let mut max_iterations:f64 = 120.0;
+    drawf(&mut canvas, &screen, max_iterations);
+    let mut line = String::new();
+    let stdin = std::io::stdin();
 
 
-    //prerequisites
-    const maxit: i32 = 2;
-    let mut draw:bool=true;
-    // let mut pixels : Vec<Arc<Mutex<HashMap<usize,Color>>>> = vec![Arc::new(Mutex::new(HashMap::from(tmp)))];
-    // let mut threads : vec<[]> = ();
-    let (sender, receiver) = mpsc::channel();
+    const SPEED:f64 = 0.03;
+    const ZOOM:f64 = 0.08;
     'main: loop {
-        //calculate
-        //draw
-        println!("HIII");
-        if draw {
-            // let pixels:[Color; (width * height) as usize] = [];
-            for i in 0..size {
-                println!("making thread for pixel {i}");
-                let senderr = sender.clone();
-                // let pixels = Arc::clone(&pixels);
-                std::thread::spawn(move || unsafe {
-                    let x = i%width;
-                    let y = i/height;
-                    let mut brightness = 0;
-
-                    for x in 0..width {
-                        for y in 0..height {
-                            let mut a = (width - 50) / 50;
-                            let mut b = (height - 50) / 50;
-
-                            let ca = a;
-                            let cb = b;
-
-                            let mut n = 0;
-                            let z = 0;
-
-                            while abs(n) < maxit {
-                                let real = a * a - b * b;
-                                let complex = 2 * a * b;
-
-                                a = real + ca;
-                                b = complex + cb;
-
-                                if abs((a + b) as c_int) > maxit {
-                                    break;
-                                }
-
-                                n += 1;
-                            }
-                            if n == 0 {
-                                brightness = 0;
-                            } else {
-                                brightness = n / 100 * 255 % 255;
-                            }
-                            if n == 80 { brightness = 255; } else if n == maxit { brightness = 0; }
-                        }
-                    }
-                    senderr.send((Point::new((i % width) as i32, (i / height) as i32), Color::RGB(brightness as u8, brightness as u8, brightness as u8))).unwrap();
-                }).join().expect("Couldn't join thread {i}");
-
-            }
-            // draw = false;
-
-            println!("Hello");
-            mem::drop(&sender);
-            let mut i = 0;
-            for received in &receiver {
-                // println!("Got also {:?}",received);
-                let (p, c) = received;
-
-                canvas.set_draw_color(c);
-                canvas.draw_point(p).unwrap();
-                canvas.clear();
-                canvas.present();
-                println!("boom");
-                i+=1;
-                if i==size {break;}
-            }
-        }
-        //get keys
+        //rect to events in event queue
         for event in events.poll_iter() {
             match event {
                 Event::Quit { .. } |
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
                     break 'main;
                 },
+                Event::KeyDown {keycode: Some(Keycode::Up), .. } => {
+                    screen.pan(0.0,(screen.right-screen.left)*SPEED);
+                    drawf(&mut canvas, &screen, max_iterations);
+                },
+                Event::KeyDown {keycode: Some(Keycode::Down), .. } => {
+                    screen.pan(0.0,(screen.right-screen.left)*-SPEED);
+                    drawf(&mut canvas, &screen, max_iterations);
+                },
+                Event::KeyDown {keycode: Some(Keycode::Left), .. } => {
+                    screen.pan((screen.right-screen.left)*-SPEED,0.0);
+                    drawf(&mut canvas, &screen, max_iterations);
+                },
+                Event::KeyDown {keycode: Some(Keycode::Right), .. } => {
+                    screen.pan((screen.right-screen.left)*SPEED,0.0);
+                    drawf(&mut canvas, &screen, max_iterations);
+                },
+                Event::MouseWheel {y:0..=i32::MAX, .. } => {
+                    screen.zoom((screen.right-screen.left)*ZOOM);
+                    drawf(&mut canvas, &screen, max_iterations);
+                }
+                Event::MouseWheel {y:i32::MIN..=0, .. } => {
+                    screen.zoom((screen.right-screen.left)*-ZOOM);
+                    drawf(&mut canvas, &screen, max_iterations);
+                },
+                Event::KeyDown {keycode: Some(Keycode::Equals), .. } => {
+                    max_iterations +=20.0;
+                    drawf(&mut canvas, &screen, max_iterations);
+                }
+                Event::KeyDown {keycode: Some(Keycode::Minus), .. } => {
+                    max_iterations -=20.0;
+                    drawf(&mut canvas, &screen, max_iterations);
+                }
+                Event::KeyDown {keycode: Some(Keycode::Z), .. } => {
+                    print!("Enter Zoom Level:");
+                    stdout().flush().unwrap();
+                    stdin.read_line(&mut line).unwrap();
+                    line.truncate(line.len()-1);
+                    println!("{:?}",line);
+                    screen.zoom= line.parse::<f64>().unwrap();
+                    line.clear();
+                    screen.zoom((screen.right-screen.left)*ZOOM*screen.zoom);
+                    drawf(&mut canvas, &screen, max_iterations);
+                }
+                Event::KeyDown {keycode: Some(Keycode::P), .. } => {
+                    print!("Enter Pan X:");
+                    stdout().flush().unwrap();
+                    stdin.read_line(&mut line).unwrap();
+                    line.truncate(line.len()-1);
+                    screen.pan.0= line.parse::<f64>().unwrap();
+                    line.clear();
+                    print!("Enter Pan Y:");
+                    stdout().flush().unwrap();
+                    stdin.read_line(&mut line).unwrap();
+                    line.truncate(line.len()-1);
+                    screen.pan.1= line.parse::<f64>().unwrap();
+                    line.clear();
+                    screen.pan((screen.right-screen.left)*SPEED*screen.pan.0,(screen.right-screen.left)*SPEED*screen.pan.1);
+                    drawf(&mut canvas, &screen, max_iterations);
+                }
+                Event::AppLowMemory { .. } => {
+                    break 'main;
+                }
                 _ => {}
             }
         }
     }
-
-
-    println!("Hello, world!");
+    println!("Goodbye");
 }
+
+//p5.js's map range function
+fn _map(n:f64,start1:f64,stop1:f64,start2:f64,stop2:f64) -> f64 {
+    return (n - start1) / (stop1 - start1) * (stop2 - start2) + start2;
+}
+
+const PRINT_POSITION: bool = true;
+const PRINT_DRAWING_STATUS: bool = false;
+const BRIGHTNESS_SCALE: f64 = 20.0;//0-20 to 0-255
+//drawfractal()
+fn drawf(canvas:&mut WindowCanvas, s: &ScreenWindow, maxit:f64) {
+    if PRINT_DRAWING_STATUS {
+        println!("Drawing fractal");
+    }
+    if PRINT_POSITION {
+        println!("{:?}",s);
+    }
+    canvas.clear();
+
+    for x in 0..WIDTH {
+        for y in 0..HEIGHT {
+            //a and b are x,y coordinates that need to be mapped to where the screen is zoomed into
+            let mut a = _map(x as f64, 0.0, WIDTH as f64, s.left, s.right);
+            let mut b = _map(y as f64, 0.0, HEIGHT as f64, s.bottom, s.top);
+
+            //copies of a and b
+            let ca = a;
+            let cb = b;
+
+            //number of iterations(it's a float so we don't have to convert
+            let mut n:f64 = 0.0;
+
+            while n < maxit {
+                let real = (a*a) - (b*b);
+                let complex = 2.0*a*b;
+
+                a = real + ca;
+                b = complex + cb;
+
+                if a.abs() + b.abs() > 100.0 {
+                    break;
+                }
+
+                n += 1.0;
+            }
+
+            let mut brightness = _map(n, 0.0, BRIGHTNESS_SCALE, 0.0, 255.0);
+            if n == maxit { brightness = 0.0; }
+            canvas.set_draw_color(Color::RGB(brightness as u8, brightness as u8, brightness as u8));
+            canvas.draw_point(Point::new(x as i32, y as i32)).unwrap();
+        }
+    }
+    canvas.present();
+    if PRINT_DRAWING_STATUS {
+        println!("done");
+    }
+    return;
+}
+
+#[derive(Debug)]
+struct ScreenWindow {
+    left:f64,
+    right:f64,
+    top:f64,
+    bottom:f64,
+    zoom:f64,     //zoom and pan are there so you can easily get back
+    pan:(f64,f64) //to where you were (x,y)
+}
+impl ScreenWindow {
+    pub fn new(l:f64,r:f64,t:f64,b:f64) -> Self {
+        Self{left:l,right:r,bottom:b,top:t,zoom:0.0,pan:(0.0,0.0)}
+    }
+    pub fn zoom(&mut self, amount:f64) {
+        self.left+=amount;
+        self.right-=amount;
+        self.top+=amount;
+        self.bottom-=amount;
+        self.zoom+=amount;
+    }
+    pub fn pan(&mut self,x:f64,y:f64) {
+        self.left+=x;
+        self.right+=x;
+        self.top+=y;
+        self.bottom+=y;
+        self.pan.0+=x;
+        self.pan.1+=y;
+    }
+}
+
